7e739b0178755c2b103684a3b9dd001234ae9739
Mubarak <66844379+MABT11@users.noreply.github.com>
2021-02-18 15:31:40 +0400

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
+//size of the array
 #define SIZE 10
-void swap(int *x, int *y)	// to swap two variables without taking a copy
+// to swap two variables without taking a copy
+void swap(int *x, int *y)	
 {
-*x = *y - *x;
-*y -= *x;
-*x += *y;
+  *x = *y - *x;
+  *y -= *x;
+  *x += *y;
 }
 void sort(int arr[SIZE])
 {
-for(int i = 0; i < SIZE; i++)
-for(int j = i; j < SIZE; j++)
-if(arr[i]>arr[j])
-swap(&arr[i], &arr[j]);
-
+  for(int i = 0; i < SIZE; i++)
+    for(int j = i; j < SIZE; j++)
+      //check if it bigger then swap
+      if(arr[i]>arr[j])
+        swap(&arr[i], &arr[j]);
 }
 int main()
 {
-int arr[SIZE];
-printf("ENTER NUMBERS IN ARRAY");
-for(int i = 0; i < SIZE; i++) scanf("%d",&arr[i]);
-sort(arr);
-for(int i = 0; i < SIZE; i++) printf("%d ",arr[i]);
+  int arr[SIZE];
+  //user input 
+  printf("ENTER NUMBERS IN ARRAY");
+  for(int i = 0; i < SIZE; i++)
+    scanf("%d",&arr[i]);
+  
+  sort(arr);
+  for(int i = 0; i < SIZE; i++)
+    printf("%d ",arr[i]);
 
-return 0;
+  return 0;
 }
